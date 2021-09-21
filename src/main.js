a4062ddd2bcaca5257d5871d75d16a6f4822e12c
clivan <1613709496@qq.com>
2021-09-21 11:19:34 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+/* 导入element-ui的css */
+import 'element-ui/lib/theme-chalk/index.css';
+/* 导入iconfont的css */
+import  '@/assets/iconfont/iconfont.css'
+import axios from 'axios'
+/* 配置请求根路径 */
+axios.defaults.baseURL = `http://127.0.0.1:8888/api/private/v1/`
+Vue.prototype.$http=axios
 Vue.config.productionTip = false
 
 new Vue({
